f90897e6c93370b482e9ba936dbf7f1bc0c172f9
rhmats <rahmatsyafriyani809@gmail.com>
2025-01-30 11:56:59 +0700

    local storage implemented

--- src/server.js
+++ src/server.js
@@ -3,6 +3,8 @@ require("dotenv").config();
 const Hapi = require("@hapi/hapi");
 const ClientError = require("./exceptions/ClientError");
 const Jwt = require("@hapi/jwt");
+const Inert = require('@hapi/inert');
+const path = require('path');
 
 // notes
 const notes = require("./api/notes");
@@ -26,15 +28,23 @@ const CollaborationsValidator = require("./validator/collaborations");
 const CollaborationsService = require("./services/postgres/CollaborationsService");
 
 // Exports
-const _exports = require('./api/exports');
-const ProducerService = require('./services/rabbitmq/ProducerService');
-const ExportsValidator = require('./validator/exports');
+const _exports = require("./api/exports");
+const ProducerService = require("./services/rabbitmq/ProducerService");
+const ExportsValidator = require("./validator/exports");
+
+// uploads
+const uploads = require("./api/uploads");
+const StorageService = require("./services/storage/StorageService");
+const UploadsValidator = require("./validator/uploads");
 
 const init = async () => {
   const collaborationsService = new CollaborationsService();
   const notesService = new NotesService(collaborationsService);
   const usersService = new UsersService();
   const authenticationsService = new AuthenticationsService();
+  const storageService = new StorageService(
+    path.resolve(__dirname, "api/uploads/files/images")
+  );
 
   const server = Hapi.server({
     port: process.env.PORT,
@@ -50,6 +60,9 @@ const init = async () => {
   await server.register([
     {
       plugin: Jwt,
+    },
+    {
+      plugin: Inert,
     },
   ]);
 
@@ -107,6 +120,13 @@ const init = async () => {
       options: {
         service: ProducerService,
         validator: ExportsValidator,
+      },
+    },
+    {
+      plugin: uploads,
+      options: {
+        service: storageService,
+        validator: UploadsValidator,
       },
     },
   ]);
